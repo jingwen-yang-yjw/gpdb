5265ed54201a5ded3ea840ec8fb7f416bcb815fb
Jingwen Yang <jingwen_yang_chn@outlook.com>
2023-05-02 16:13:20 +0800

    This commit includes two changes: - If non-grouping expressions for partial_rel are plain vars outside an aggregate,   it's unsafe to make foreign grouping.   So in this commit, we disable such aggregates pushdown.
    - Add more test cases about aggregates.

--- contrib/postgres_fdw/sql/postgres_sql/mpp_gp2pg_postgres_init.sql
+++ contrib/postgres_fdw/sql/postgres_sql/mpp_gp2pg_postgres_init.sql
@@ -9,15 +9,27 @@ SET timezone = 'PST8PDT';
 CREATE SCHEMA "MPP_S 1";
 CREATE TABLE "MPP_S 1"."T 1" (
 	c1 int,
-	c2 int
+	c2 int,
+	c3 smallint,
+	c4 bigint,
+	c5 real,
+	c6 double precision,
+	c7 numeric
 );
 
 -- Disable autovacuum for these tables to avoid unexpected effects of that
 ALTER TABLE "MPP_S 1"."T 1" SET (autovacuum_enabled = 'false');
 
-INSERT INTO "MPP_S 1"."T 1"
+INSERT INTO "MPP_S 1"."T 1" (c1, c2, c3, c4)
 	SELECT id,
-	       id % 10
+	       id % 10,
+	       id,
+	       id
 	FROM generate_series(1, 1000) id;
+
+UPDATE "MPP_S 1"."T 1"
+  SET c5 = c1 * 0.001,
+      c6 = c1 * 0.001,
+      c7 = c1 * 0.001;
 
 ANALYZE "MPP_S 1"."T 1";
